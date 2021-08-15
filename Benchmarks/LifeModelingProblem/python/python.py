9e6140d2fd662ec2fb36a3e21721f25484029264
Alec Loudenback <alecloudenback@gmail.com>
2021-08-14 23:00:51 -0500

    update python code to have separate setups and runs
    primarily to have a `q` and `w` that were not numpy arrays, because using `numpy` for the base Python accumulator was 4x slower:
    
    Prior code:
    ```
    13.979937791 # numpy vectorized
    2.370869667000001 # python base
    ```
    
    Your commit (dropping the microseconds):
    ```
    Numpy Vectorized: 14.186
    Python Accumulator: 9.876
    Numba Accumulator: 0.649
    ```
    The timing of the second one seemed off... it seems like it's 4x faster to not use numpy arrays, so I went back to that:
    ```
    Numpy Vectorized: 14.261
    Base Python Accumulator: 2.314
    Numpy Vectorized: 0.626
    ```

--- Benchmarks/LifeModelingProblem/python/python.py
+++ Benchmarks/LifeModelingProblem/python/python.py
@@ -1,9 +1,7 @@
-
 import timeit
 
-setup='''
+setup_numpy='''
 import numpy as np
-from numba import njit
 
 q = np.array([0.001, 0.002, 0.003, 0.003, 0.004, 0.004, 0.005, 0.007, 0.009, 0.011])
 w = np.array([0.05, 0.07, 0.08, 0.10, 0.14, 0.20, 0.20, 0.20, 0.10, 0.04])
@@ -24,20 +22,51 @@ def npv_vec(q, w, P, S, r):
     return np.sum(ncf * d)
 
 
+'''
+bm_res_vec = timeit.timeit(stmt='''npv_vec(q, w, P, S, r)''', setup=setup_numpy, number=1000000)
+print(f"Numpy Vectorized: {bm_res_vec:0.3f}")
+
+setup_base='''
+import numpy as np
+from numba import njit
+
+q = [0.001, 0.002, 0.003, 0.003, 0.004, 0.004, 0.005, 0.007, 0.009, 0.011]
+w = [0.05, 0.07, 0.08, 0.10, 0.14, 0.20, 0.20, 0.20, 0.10, 0.04]
+P = 100
+S = 25000
+r = 0.02
+
+
+
 # Python Accumulator, via Houstonwp
-def npv_loop(q, w, P, S, r):
+def npv_base(q, w, P, S, r):
     inforce = 1.0
     result = 0.0
     v = 1 / (1 + r)
     v_t = v
-    for t in range(0, len(q)):
+    for t in range(0, len(q)-1):
         result = result + inforce * (P-S*q[t]) * v_t
         inforce = inforce * (1 - (q[t] + w[t]))
         v_t = v_t * v
     return result
+'''
+bm_res_base = timeit.timeit(stmt='''npv_base(q, w, P, S, r)''', setup=setup_base, number=1000000)
+print(f"Base Python Accumulator: {bm_res_base:0.3f}")
 
 
-# Numba Accumulator
+
+setup_numba='''
+import numpy as np
+from numba import njit
+
+q = np.array([0.001, 0.002, 0.003, 0.003, 0.004, 0.004, 0.005, 0.007, 0.009, 0.011])
+w = np.array([0.05, 0.07, 0.08, 0.10, 0.14, 0.20, 0.20, 0.20, 0.10, 0.04])
+P = 100
+S = 25000
+r = 0.02
+
+
+# Numba Accumulator, per Dimitar V.
 @njit
 def npv_numba(q, w, P, S, r):
     inforce = 1.0
@@ -49,17 +78,7 @@ def npv_numba(q, w, P, S, r):
         inforce = inforce * (1 - (q[t] + w[t]))
         v_t = v_t * v
     return result
+
 '''
-
-benchmark_vec = '''npv_vec(q, w, P, S, r)'''
-benchmark_loop = '''npv_loop(q, w, P, S, r)'''
-benchmark_numba = '''npv_numba(q, w, P, S, r)'''
-
-bm_res_vec = timeit.timeit(stmt=benchmark_vec, setup=setup, number=1000000)
-print(f"Numpy Vectorized: {bm_res_vec:0.3f} μs")
-
-bm_res_loop = timeit.timeit(stmt=benchmark_loop, setup=setup, number=1000000)
-print(f"Python Accumulator: {bm_res_loop:0.3f} μs")
-
-bm_res_numba = timeit.timeit(stmt=benchmark_numba, setup=setup, number=1000000)
-print(f"Numba Accumulator: {bm_res_numba:0.3f} μs")
+bm_res_numba = timeit.timeit(stmt='''npv_numba(q, w, P, S, r)''', setup=setup_numba, number=1000000)
+print(f"Numpy Vectorized: {bm_res_numba:0.3f}")
