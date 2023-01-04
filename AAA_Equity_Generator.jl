2c2df8b4d1341080500cc989a61ae71737bb7ccc
Alec Loudenback <alecloudenback@gmail.com>
2023-01-03 22:53:46 -0600

    incorporate all four equity funds

--- AAA_Equity_Generator.jl
+++ AAA_Equity_Generator.jl
@@ -10,8 +10,8 @@ using Distributions
 # ╔═╡ 96c6047b-4cf1-4be0-a803-2bf57368cf6c
 using Random
 
-# ╔═╡ 482b68b4-f3ab-4ccb-a3a8-c789bbf14194
-using StatsBase
+# ╔═╡ 05ba01cc-8b80-4d36-b478-74457c014476
+using LabelledArrays
 
 # ╔═╡ 5cf21c5b-44e8-4319-9a0c-5770d823b5f7
 md" Initial attempt to replicate AAA equtity generator. 
@@ -24,83 +24,141 @@ Todo for prototype:
 
 Reference: [https://www.actuary.org/sites/default/files/pdf/life/c3supp_march05.pdf](https://www.actuary.org/sites/default/files/pdf/life/c3supp_march05.pdf)"
 
-# ╔═╡ fb638ee3-cddb-47d7-b573-1db20ccd1fcf
-params = (;
-	τ = 0.12515,
-	ϕ = 0.35229,
-	σ_v = 0.32645,
-	ρ = -.2488,
-	A = 0.055,
-	B = 0.56,
-	C  = -0.9,
-	σ_0 = 0.1476,
-	σ_m = 0.0305,
-	σ_p = 0.3,
-	σ⃰ = 0.7988,
-	months = 12*100,
+# ╔═╡ 067a7524-ef60-4ebd-be38-54459c2ea7d6
+md"## Model Parameters"
+
+# ╔═╡ 008699e5-03de-431c-8c0a-f36825627f3b
+# use a labelled array for easy reference of the parameters 
+params = @LArray [
+    0.12515 0.14506 0.16341 0.20201 # τ
+    0.35229 0.41676 0.3632 0.35277 # ϕ
+    0.32645 0.32634 0.35789 0.34302 # σ_v
+    -0.2488 -0.1572 -0.2756 -0.2843 #ρ
+    0.055 0.055 0.055 0.055 # A
+    0.56 0.466 0.67 0.715 # B
+    -0.9 -0.9 -0.95 -1.0 #C
+    0.1476 0.1688 0.2049 0.2496 # σ_0
+    0.0305 0.0354 0.0403 0.0492 # σ_m
+    0.3 0.3 0.4 0.55 # σ_p
+    0.7988 0.4519 0.9463 1.1387 # σ⃰
+] ( # define the regions each label refers to
+	τ = (1,:),
+	ϕ = (2,:),
+	σ_v = (3,:),
+	ρ = (4,:),
+	A = (5,:),
+	B = (6,:),
+	C = (7,:),
+	σ_0 = (8,:),
+	σ_m = (9,:),
+	σ_p = (10,:),
+	σ⃰ = (11,:)
 )
+
+# ╔═╡ d04a7924-39c7-42a6-8068-02b9987dce54
+md"## Stochastic Log Volatility Model
+Note that the `@.` and other broadcasting (`.` symbol) allows us to operate on multiple funds at once.
+"
 
 # ╔═╡ a2d95991-9f15-46b2-9d01-84bc71551bb9
 function v(v_prior,params,Zₜ) 
 	(;σ_v, σ_m,σ_p,σ⃰,ϕ,τ) = params
 	
-	v_m = log(σ_m)
-	v_p = log(σ_p)
-	v⃰ = log(σ⃰)
+	v_m = log.(σ_m)
+	v_p = log.(σ_p)
+	v⃰ = log.(σ⃰)
 
-	ṽ = min(v_p, (1-ϕ)*v_prior + ϕ*log(τ) ) + σ_v * Zₜ[2]
+	# vol are the odd values in the random array
+	ṽ =  @. min(v_p, (1 - ϕ) * v_prior + ϕ * log(τ) ) + σ_v * Zₜ[[1,3,5,7]]
 	
-	v = max(v_m, min(v⃰,ṽ))
+	v = @. max(v_m, min(v⃰,ṽ))
 
 	return v
 end
 
 # ╔═╡ b089355b-852b-4649-bb0b-b3ac2454f00e
-function scenario(params)
-	(;σ_v,σ_0, ρ,A,B,C,months) = params
+function scenario(params,Z;months=1200)
+	(;σ_v,σ_0, ρ,A,B,C) = params
 
-	Z = MvNormal(
-		[0.,0.], #means for return and volatility
-		[1. ρ; ρ 1.] # covariance matrix
-		# full covariance matrix in AAA Excel workook on Parameters tab
-	)
-	Zₜ = rand(Z)
-
-	v_t = log(σ_0)
+	n_funds = size(params,2)
 	
-	map(1:months) do t
+	#initilize/pre-allocate
+	Zₜ = rand(Z)
+	v_t = log.(σ_0)
+	σ_t = zeros(n_funds)
+	μ_t = zeros(n_funds)
+	
+	log_returns = map(1:months) do t
 		Zₜ = rand!(Z,Zₜ)
+		v_t .= v(v_t,params,Zₜ)
 
-		v_t = v(v_t,params,Zₜ)
+		σ_t .= exp.(v_t)
 
-		σ_t = exp(v_t)
+		@. μ_t =  A + B * σ_t + C * (σ_t)^2
 
-		μ_t = A + B * σ_t + C * (σ_t)^2
-
-		log_return = μ_t / 12 + σ_t / sqrt(12) * Zₜ[1]
+		# return are the even values in the random array
+		log_return = @. μ_t / 12 + σ_t / sqrt(12) * Zₜ[[2,4,6,8]]
 	end
+
+	# convert vector of vector to matrix
+	reduce(hcat,log_returns)
 		
 		
 		
 end
 
-# ╔═╡ 0b85a138-827e-11ed-05ad-5daf08af6bee
-x =scenario(params)
+# ╔═╡ 256e10bf-89e1-4f8e-aacb-ec161447cff3
+md" ## Scenarios and validation
+
+### A single scenario"
+
+# ╔═╡ 38d7927f-aac9-466c-ba4b-be2efe39d883
+md"### Validation of summary statistics"
 
 # ╔═╡ 81405282-74f7-497f-9b56-59a03fe3c407
-md"The summary statistics expected:
+md"The summary statistics expected (per paper Table 8):
 
-- μ ≈ 0.0060
-- σ ≈ 0.0436
+- `μ ≈ [0.0060, 0.0062, 0.0063, 0.0065]`
+- `σ ≈ [0.0436, 0.0492, 0.0590, 0.0724]`
 "
+
+# ╔═╡ bc0dde6d-8df9-4d1d-bec5-1f668f173b5b
+md"## The Multivariate normal and covariance matrix"
+
+# ╔═╡ 997a67f2-bafe-4618-9abc-5b9b87e607f7
+# 11 columns because it's got the bond returns in it
+cov_matrix = [
+	1.000	-0.249	0.318	-0.082	0.625	-0.169	0.309	-0.183	0.023	0.075	0.080;
+	-0.249	1.000	-0.046	0.630	-0.123	0.829	-0.136	0.665	-0.120	0.192	0.393;
+	0.318	-0.046	1.000	-0.157	0.259	-0.050	0.236	-0.074	-0.066	0.034	0.044;
+	-0.082	0.630	-0.157	1.000	-0.063	0.515	-0.098	0.558	-0.105	0.130	0.234;
+	0.625	-0.123	0.259	-0.063	1.000	-0.276	0.377	-0.180	0.034	0.028	0.054;
+	-0.169	0.829	-0.050	0.515	-0.276	1.000	-0.142	0.649	-0.106	0.067	0.267;
+	0.309	-0.136	0.236	-0.098	0.377	-0.142	1.000	-0.284	0.026	0.006	0.045;
+	-0.183	0.665	-0.074	0.558	-0.180	0.649	-0.284	1.000	0.034	-0.091	-0.002;
+	0.023	-0.120	-0.066	-0.105	0.034	-0.106	0.026	0.034	1.000	0.047	-0.028;
+	0.075	0.192	0.034	0.130	0.028	0.067	0.006	-0.091	0.047	1.000	0.697;
+	0.080	0.393	0.044	0.234	0.054	0.267	0.045	-0.002	-0.028	0.697	1.000;
+]
+
+# ╔═╡ 0c1c7cd8-b154-48da-931f-44e0f0b0c840
+	Z = MvNormal(
+		zeros(11), #means for return and volatility
+		cov_matrix # covariance matrix
+		# full covariance matrix in AAA Excel workook on Parameters tab
+	)
+
+
+# ╔═╡ 0b85a138-827e-11ed-05ad-5daf08af6bee
+x =scenario(params,Z;months=10)
 
 # ╔═╡ 4e1a31ce-8164-4c00-bc68-ceefc7ccff3a
 let
 	# generate 1000 scenarios and calculate summary statisics
-	scens = [scenario(params) for _ in 1:1000]
+	scens = [scenario(params,Z) for _ in 1:1000]
 	
-	μ = mean(mean(x) for x in scens)
-	σ = mean(std(x) for x in scens)
+	μ = mean(mean(x,dims=2) for x in scens)
+	σ = mean(std(x,dims=2) for x in scens)
 	(;μ,σ)
 end
 
@@ -108,12 +166,12 @@ end
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
 Distributions = "31c24e10-a181-5473-b8eb-7969acd0382f"
+LabelledArrays = "2ee39098-c373-598a-b85f-a56591580800"
 Random = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
-StatsBase = "2913bbd2-ae8a-5f71-8c99-4fb6c76f3a91"
 
 [compat]
 Distributions = "~0.25.79"
-StatsBase = "~0.33.21"
+LabelledArrays = "~1.12.5"
 """
 
 # ╔═╡ 00000000-0000-0000-0000-000000000002
@@ -122,11 +180,41 @@ PLUTO_MANIFEST_TOML_CONTENTS = """
 
 julia_version = "1.8.3"
 manifest_format = "2.0"
-project_hash = "94454ce253e789671e3d75829a5be1163393c936"
+project_hash = "17c32bf94553289ec5c4a652ae069ebb0f59e840"
+
+[[deps.Adapt]]
+deps = ["LinearAlgebra"]
+git-tree-sha1 = "195c5505521008abea5aee4f96930717958eac6f"
+uuid = "79e6a3ab-5dfb-504d-930d-738a2a938a0e"
+version = "3.4.0"
 
 [[deps.ArgTools]]
 uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
 version = "1.1.1"
+
+[[deps.ArrayInterface]]
+deps = ["ArrayInterfaceCore", "Compat", "IfElse", "LinearAlgebra", "Static"]
+git-tree-sha1 = "6d0918cb9c0d3db7fe56bea2bc8638fc4014ac35"
+uuid = "4fba245c-0d91-5ea0-9b3e-6abc04ee57a9"
+version = "6.0.24"
+
+[[deps.ArrayInterfaceCore]]
+deps = ["LinearAlgebra", "SparseArrays", "SuiteSparse"]
+git-tree-sha1 = "badccc4459ffffb6bce5628461119b7057dec32c"
+uuid = "30b0a656-2188-435a-8636-2ec0e6a096e2"
+version = "0.1.27"
+
+[[deps.ArrayInterfaceStaticArrays]]
+deps = ["Adapt", "ArrayInterface", "ArrayInterfaceCore", "ArrayInterfaceStaticArraysCore", "LinearAlgebra", "Static", "StaticArrays"]
+git-tree-sha1 = "f12dc65aef03d0a49650b20b2fdaf184928fd886"
+uuid = "b0d46f97-bff5-4637-a19a-dd75974142cd"
+version = "0.1.5"
+
+[[deps.ArrayInterfaceStaticArraysCore]]
+deps = ["Adapt", "ArrayInterfaceCore", "LinearAlgebra", "StaticArraysCore"]
+git-tree-sha1 = "93c8ba53d8d26e124a5a8d4ec914c3a16e6a0970"
+uuid = "dd5226c6-a4d4-4bc7-8575-46859f9c95b9"
+version = "0.1.3"
 
 [[deps.Artifacts]]
 uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
@@ -152,6 +240,12 @@ git-tree-sha1 = "38f7a08f19d8810338d4f5085211c7dfa5d5bdd8"
 uuid = "9e997f8a-9a97-42d5-a9f1-ce6bfc15e2c0"
 version = "0.1.4"
 
+[[deps.CommonSubexpressions]]
+deps = ["MacroTools", "Test"]
+git-tree-sha1 = "7b8a93dba8af7e3b42fecabf646260105ac373f7"
+uuid = "bbf7d656-a473-5ed7-a52c-81e309532950"
+version = "0.3.0"
+
 [[deps.Compat]]
 deps = ["Dates", "LinearAlgebra", "UUIDs"]
 git-tree-sha1 = "00a2cccc7f098ff3b66806862d275ca3db9e6e5a"
@@ -174,6 +268,11 @@ git-tree-sha1 = "d1fff3a548102f48987a52a2e0d114fa97d730f0"
 uuid = "864edb3b-99cc-5e75-8d2d-829cb0a9cfe8"
 version = "0.18.13"
 
+[[deps.DataValueInterfaces]]
+git-tree-sha1 = "bfc1187b79289637fa0ef6d4436ebdfe6905cbd6"
+uuid = "e2d170a0-9d28-54be-80f0-106bbe20a464"
+version = "1.0.0"
+
 [[deps.Dates]]
 deps = ["Printf"]
 uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
@@ -183,6 +282,18 @@ deps = ["InverseFunctions", "Test"]
 git-tree-sha1 = "80c3e8639e3353e5d2912fb3a1916b8455e2494b"
 uuid = "b429d917-457f-4dbc-8f4c-0cc954292b1d"
 version = "0.4.0"
+
+[[deps.DiffResults]]
+deps = ["StaticArraysCore"]
+git-tree-sha1 = "782dd5f4561f5d267313f23853baaaa4c52ea621"
+uuid = "163ba53b-c6d8-5494-b064-1a9d43ac40c5"
+version = "1.1.0"
+
+[[deps.DiffRules]]
+deps = ["IrrationalConstants", "LogExpFunctions", "NaNMath", "Random", "SpecialFunctions"]
+git-tree-sha1 = "c5b6685d53f933c11404a3ae9822afe30d522494"
+uuid = "b552c78f-8df3-52c6-915a-8e097449b14b"
+version = "1.12.2"
 
 [[deps.Distributions]]
 deps = ["ChainRulesCore", "DensityInterface", "FillArrays", "LinearAlgebra", "PDMats", "Printf", "QuadGK", "Random", "SparseArrays", "SpecialFunctions", "Statistics", "StatsBase", "StatsFuns", "Test"]
@@ -216,11 +327,28 @@ git-tree-sha1 = "9a0472ec2f5409db243160a8b030f94c380167a3"
 uuid = "1a297f60-69ca-5386-bcde-b61e274b549b"
 version = "0.13.6"
 
+[[deps.ForwardDiff]]
+deps = ["CommonSubexpressions", "DiffResults", "DiffRules", "LinearAlgebra", "LogExpFunctions", "NaNMath", "Preferences", "Printf", "Random", "SpecialFunctions", "StaticArrays"]
+git-tree-sha1 = "a69dd6db8a809f78846ff259298678f0d6212180"
+uuid = "f6369f11-7733-5829-9624-2563aa707210"
+version = "0.10.34"
+
+[[deps.GPUArraysCore]]
+deps = ["Adapt"]
+git-tree-sha1 = "6872f5ec8fd1a38880f027a26739d42dcda6691f"
+uuid = "46192b85-c4d5-4398-a991-12ede77f4527"
+version = "0.1.2"
+
 [[deps.HypergeometricFunctions]]
 deps = ["DualNumbers", "LinearAlgebra", "OpenLibm_jll", "SpecialFunctions", "Test"]
 git-tree-sha1 = "709d864e3ed6e3545230601f94e11ebc65994641"
 uuid = "34004b35-14d8-5ef3-9330-4cdb6864b03a"
 version = "0.3.11"
+
+[[deps.IfElse]]
+git-tree-sha1 = "debdd00ffef04665ccbb3e150747a77560e8fad1"
+uuid = "615f187c-cbe4-4ef1-ba3b-2fcf58d6d173"
+version = "0.1.1"
 
 [[deps.InteractiveUtils]]
 deps = ["Markdown"]
@@ -237,11 +365,22 @@ git-tree-sha1 = "7fd44fd4ff43fc60815f8e764c0f352b83c49151"
 uuid = "92d709cd-6900-40b7-9082-c6be49f344b6"
 version = "0.1.1"
 
+[[deps.IteratorInterfaceExtensions]]
+git-tree-sha1 = "a3f24677c21f5bbe9d2a714f95dcd58337fb2856"
+uuid = "82899510-4779-5014-852e-03e436cf321d"
+version = "1.0.0"
+
 [[deps.JLLWrappers]]
 deps = ["Preferences"]
 git-tree-sha1 = "abc9885a7ca2052a736a600f7fa66209f96506e1"
 uuid = "692b3bcd-3c85-4b1f-b108-f13ce0eb3210"
 version = "1.4.1"
+
+[[deps.LabelledArrays]]
+deps = ["ArrayInterfaceCore", "ArrayInterfaceStaticArrays", "ArrayInterfaceStaticArraysCore", "ChainRulesCore", "ForwardDiff", "LinearAlgebra", "MacroTools", "PreallocationTools", "RecursiveArrayTools", "StaticArrays"]
+git-tree-sha1 = "dae002226b59701dbafd7e2dd757df1bd83442fd"
+uuid = "2ee39098-c373-598a-b85f-a56591580800"
+version = "1.12.5"
 
 [[deps.LibCURL]]
 deps = ["LibCURL_jll", "MozillaCACerts_jll"]
@@ -277,6 +416,12 @@ version = "0.3.19"
 
 [[deps.Logging]]
 uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
+
+[[deps.MacroTools]]
+deps = ["Markdown", "Random"]
+git-tree-sha1 = "42324d08725e200c23d4dfb549e0d5d89dede2d2"
+uuid = "1914dd2f-81c6-5fcd-8719-6d5c9610ff09"
+version = "0.5.10"
 
 [[deps.Markdown]]
 deps = ["Base64"]
@@ -339,6 +484,12 @@ deps = ["Artifacts", "Dates", "Downloads", "LibGit2", "Libdl", "Logging", "Markd
 uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
 version = "1.8.0"
 
+[[deps.PreallocationTools]]
+deps = ["Adapt", "ArrayInterfaceCore", "ForwardDiff"]
+git-tree-sha1 = "2c88339bcfa011089f7538f89c5be780d0b558bb"
+uuid = "d236fae5-4411-538c-8e31-a6e3d9e00b46"
+version = "0.4.6"
+
 [[deps.Preferences]]
 deps = ["TOML"]
 git-tree-sha1 = "47e5f437cc0e7ef2ce8406ce1e7e24d44915f88d"
@@ -362,6 +513,18 @@ uuid = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
 [[deps.Random]]
 deps = ["SHA", "Serialization"]
 uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+
+[[deps.RecipesBase]]
+deps = ["SnoopPrecompile"]
+git-tree-sha1 = "18c35ed630d7229c5584b945641a73ca83fb5213"
+uuid = "3cdcf5f2-1ef4-517c-9805-6587b60abb01"
+version = "1.3.2"
+
+[[deps.RecursiveArrayTools]]
+deps = ["Adapt", "ArrayInterfaceCore", "ArrayInterfaceStaticArraysCore", "ChainRulesCore", "DocStringExtensions", "FillArrays", "GPUArraysCore", "IteratorInterfaceExtensions", "LinearAlgebra", "RecipesBase", "StaticArraysCore", "Statistics", "SymbolicIndexingInterface", "Tables", "ZygoteRules"]
+git-tree-sha1 = "53d040e68b5afff59a9eb1f692d9a08369b07f61"
+uuid = "731186ca-8d62-57ce-b412-fbd966d074cd"
+version = "2.34.0"
 
 [[deps.Reexport]]
 git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
@@ -387,6 +550,11 @@ version = "0.7.0"
 [[deps.Serialization]]
 uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
 
+[[deps.SnoopPrecompile]]
+git-tree-sha1 = "f604441450a3c0569830946e5b33b78c928e1a85"
+uuid = "66db9d55-30c0-4569-8b51-7e840670fc0c"
+version = "1.0.1"
+
 [[deps.Sockets]]
 uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
 
@@ -405,6 +573,23 @@ deps = ["ChainRulesCore", "IrrationalConstants", "LogExpFunctions", "OpenLibm_jl
 git-tree-sha1 = "d75bda01f8c31ebb72df80a46c88b25d1c79c56d"
 uuid = "276daf66-3868-5448-9aa4-cd146d93841b"
 version = "2.1.7"
+
+[[deps.Static]]
+deps = ["IfElse"]
+git-tree-sha1 = "c35b107b61e7f34fa3f124026f2a9be97dea9e1c"
+uuid = "aedffcd0-7271-4cad-89d0-dc628f76c6d3"
+version = "0.8.3"
+
+[[deps.StaticArrays]]
+deps = ["LinearAlgebra", "Random", "StaticArraysCore", "Statistics"]
+git-tree-sha1 = "ffc098086f35909741f71ce21d03dadf0d2bfa76"
+uuid = "90137ffa-7385-5640-81b9-e52037218182"
+version = "1.5.11"
+
+[[deps.StaticArraysCore]]
+git-tree-sha1 = "6b7ba252635a5eff6a0b0664a41ee140a1c9e72a"
+uuid = "1e83bf80-4336-4d27-bf5d-d5a4f845583c"
+version = "1.4.0"
 
 [[deps.Statistics]]
 deps = ["LinearAlgebra", "SparseArrays"]
@@ -432,10 +617,28 @@ version = "1.1.1"
 deps = ["Libdl", "LinearAlgebra", "Serialization", "SparseArrays"]
 uuid = "4607b0f0-06f3-5cda-b6b1-a6196a1729e9"
 
+[[deps.SymbolicIndexingInterface]]
+deps = ["DocStringExtensions"]
+git-tree-sha1 = "6b764c160547240d868be4e961a5037f47ad7379"
+uuid = "2efcf032-c050-4f8e-a9bb-153293bab1f5"
+version = "0.2.1"
+
 [[deps.TOML]]
 deps = ["Dates"]
 uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
 version = "1.0.0"
+
+[[deps.TableTraits]]
+deps = ["IteratorInterfaceExtensions"]
+git-tree-sha1 = "c06b2f539df1c6efa794486abfb6ed2022561a39"
+uuid = "3783bdb8-4a98-5b6b-af9a-565f29a5fe9c"
+version = "1.0.1"
+
+[[deps.Tables]]
+deps = ["DataAPI", "DataValueInterfaces", "IteratorInterfaceExtensions", "LinearAlgebra", "OrderedCollections", "TableTraits", "Test"]
+git-tree-sha1 = "c79322d36826aa2f4fd8ecfa96ddb47b174ac78d"
+uuid = "bd369af6-aec1-5ad0-b16a-f7cc5008161c"
+version = "1.10.0"
 
 [[deps.Tar]]
 deps = ["ArgTools", "SHA"]
@@ -458,6 +661,12 @@ deps = ["Libdl"]
 uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
 version = "1.2.12+3"
 
+[[deps.ZygoteRules]]
+deps = ["MacroTools"]
+git-tree-sha1 = "8c1a8e4dfacb1fd631745552c8db35d0deb09ea0"
+uuid = "700de1a5-db45-46bc-99cf-38207098b444"
+version = "0.2.2"
+
 [[deps.libblastrampoline_jll]]
 deps = ["Artifacts", "Libdl", "OpenBLAS_jll"]
 uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
@@ -478,12 +687,19 @@ version = "17.4.0+0"
 # ╟─5cf21c5b-44e8-4319-9a0c-5770d823b5f7
 # ╠═0d04723f-2b88-4467-9b7b-fbbf1a29f88c
 # ╠═96c6047b-4cf1-4be0-a803-2bf57368cf6c
-# ╠═482b68b4-f3ab-4ccb-a3a8-c789bbf14194
-# ╠═fb638ee3-cddb-47d7-b573-1db20ccd1fcf
+# ╠═05ba01cc-8b80-4d36-b478-74457c014476
+# ╟─067a7524-ef60-4ebd-be38-54459c2ea7d6
+# ╠═008699e5-03de-431c-8c0a-f36825627f3b
+# ╠═d04a7924-39c7-42a6-8068-02b9987dce54
 # ╠═a2d95991-9f15-46b2-9d01-84bc71551bb9
 # ╠═b089355b-852b-4649-bb0b-b3ac2454f00e
+# ╟─256e10bf-89e1-4f8e-aacb-ec161447cff3
 # ╠═0b85a138-827e-11ed-05ad-5daf08af6bee
+# ╟─38d7927f-aac9-466c-ba4b-be2efe39d883
 # ╟─81405282-74f7-497f-9b56-59a03fe3c407
 # ╠═4e1a31ce-8164-4c00-bc68-ceefc7ccff3a
+# ╠═bc0dde6d-8df9-4d1d-bec5-1f668f173b5b
+# ╠═0c1c7cd8-b154-48da-931f-44e0f0b0c840
+# ╠═997a67f2-bafe-4618-9abc-5b9b87e607f7
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
